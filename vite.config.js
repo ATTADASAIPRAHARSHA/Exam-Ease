4d84447d343e5bb0d7dc05da593829d1def614a3
ATTADASAIPRAHARSHA <saipraharshaattada@gmail.com>
2024-08-19 12:55:46 +0530

    go

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),require('tailwindcss'),
+    require('autoprefixer'),],
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
