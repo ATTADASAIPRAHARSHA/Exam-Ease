56db5e2bc5e4a3104bcceffd91e1faa22cf9320b
ATTADASAIPRAHARSHA <saipraharshaattada@gmail.com>
2024-08-19 13:02:37 +0530

    check

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+/**@type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',
+    './public/**/*.html'
+  ],
   theme: {
     extend: {},
   },
